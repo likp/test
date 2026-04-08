2041ad9d602763ac86ffe977026651196e96de65
Petros Likidis <petros.likidis@scb.se>
2026-04-08 13:37:47 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 git config --global --add http.https://devopstest.scb.intra/SCB/SCB/_git/.extraHeader "AUTHORIZATION: Basic $(printf ":$GIT_PERSONAL_ACCESS_TOKEN" | base64 -w0)"
+
+sudo apt-get update
+sudo apt-get install -y curl gnupg apt-transport-https
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-prod.gpg > /dev/null
+sudo curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list \
+    -o /etc/apt/sources.list.d/mssql-release.list
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+sudo apt-get install -y unixodbc unixodbc-dev krb5-user
+pip install pyodbc
+
+
+sudo debconf-set-selections <<< "krb5-config krb5-config/default_realm string SCB.INTRA"
+sudo debconf-set-selections <<< "krb5-config krb5-config/kerberos_servers string SCB.INTRA"
+sudo debconf-set-selections <<< "krb5-config krb5-config/admin_server string SCB.INTRA"
+
+sudo apt-get install -y krb5-user
