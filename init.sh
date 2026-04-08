7c04bd5064fc7d008e96d21e2f74b80b8cf0351d
Petros Likidis <petros.likidis@scb.se>
2026-04-08 13:50:50 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -12,11 +12,11 @@ sudo curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list \
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 sudo apt-get install -y unixodbc unixodbc-dev krb5-user
+
+#sudo debconf-set-selections <<< "krb5-config krb5-config/default_realm string SCB.INTRA"
+#sudo debconf-set-selections <<< "krb5-config krb5-config/kerberos_servers string SCB.INTRA"
+#sudo debconf-set-selections <<< "krb5-config krb5-config/admin_server string SCB.INTRA"
+
+#sudo apt-get install -y krb5-user
+
 pip install pyodbc
-
-
-sudo debconf-set-selections <<< "krb5-config krb5-config/default_realm string SCB.INTRA"
-sudo debconf-set-selections <<< "krb5-config krb5-config/kerberos_servers string SCB.INTRA"
-sudo debconf-set-selections <<< "krb5-config krb5-config/admin_server string SCB.INTRA"
-
-sudo apt-get install -y krb5-user
